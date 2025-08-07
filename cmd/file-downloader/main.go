4d286a82dc16578d387ea5b39b8336fdfeb82dd4
Ilya Yusov <yusovilya@gmail.com>
2025-08-07 22:09:13 +0300

    добавил логгер

--- cmd/file-downloader/main.go
+++ cmd/file-downloader/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/YusovID/file-downloader-06.08.2025/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func init() {
@@ -16,11 +22,33 @@ func init() {
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
-	// TODO: init logger
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting file-downloader", slog.String("env", cfg.Env))
 
 	// TODO: init router
 
 	// TODO: init server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
